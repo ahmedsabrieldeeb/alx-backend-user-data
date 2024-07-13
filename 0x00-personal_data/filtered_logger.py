1493a9cf87a28bcd9f760cd824a3edfcccce877c
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-07-13 13:07:46 +0300

    chore: Add RedactingFormatter class for obfuscating sensitive data in log messages

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,7 @@
 """
 This module contains a function for obfuscating sensitive data in log messages
 """
-
+import logging
 from typing import List
 import re
 
@@ -25,3 +25,28 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f"{field}=.*?{separator}",
                          f"{field}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Redacting Formatter constructor """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format the record
+
+        Args:
+            record (logging.LogRecord): The record to format
+
+        Returns:
+            str: The formatted record
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
