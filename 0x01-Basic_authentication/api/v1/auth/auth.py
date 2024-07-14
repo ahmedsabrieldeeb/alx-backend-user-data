f851ab254950e6089740c5113d6603b1abd6e89d
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-07-14 10:42:27 +0300

    Added the functionality of `require_auth()` method in `Auth` class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,16 @@ class Auth:
     """ Auth class Tempate """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require_auth method """
-        return False
+        if path is None or excluded_paths is None:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header method """
