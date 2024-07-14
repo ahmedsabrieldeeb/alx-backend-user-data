04eaf5115697ee85297600828894763e0bcc84ce
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-07-14 12:40:34 +0300

    styling issues

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,10 +8,10 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """ Extracting the base64 part of Authorization header
-        
+
         Args:
             authorization_header (str): Authorization header
-        
+
         Returns:
             str: base64 part of Authorization header
             None: otherwise
