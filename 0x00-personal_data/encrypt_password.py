fdee14868a2a8669a5f41d560a02be91cf759901
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-07-14 03:13:22 +0300

    Implemented `hash_password()` function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" This module contains a function for hashing passwords """
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password
+
+    Args:
+        password (str): The password to hash
+
+    Returns:
+        bytes: The hashed password
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
