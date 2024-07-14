9886e5da37db59d00a43be63a7164507852a4d4f
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-07-14 10:34:24 +0300

    Impleneted the basic Auth template class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,1 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Module of Auth classes
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Auth class Tempate """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth method """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header method """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
+        """ current_user method """
+        return None
