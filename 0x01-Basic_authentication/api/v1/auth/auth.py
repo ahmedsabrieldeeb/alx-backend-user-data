03177062da5fd2e8dfc415d7a3e5505620e7bab6
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-07-14 11:22:40 +0300

    Implemented `before_request()` function handling each request

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,13 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header method """
-        return None
+        if request is None:
+            return None
+
+        try:
+            return request.headers.get('Authorization')
+        except Exception:
+            return None
 
     def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """ current_user method """
