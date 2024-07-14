a171603d5157aa7f519b3f4aff0efb28ece91638
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-07-14 11:59:47 +0300

    put the auth_type validation in the global space

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,6 +15,16 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 
+auth_type = getenv("AUTH_TYPE", "auth")
+if auth_type == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -56,14 +66,4 @@ def before_request():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    auth_type = getenv("AUTH_TYPE", "auth")
-
-    if auth_type == "auth":
-        from api.v1.auth.auth import Auth
-        auth = Auth()
-
-    if auth_type == "basic_auth":
-        from api.v1.auth.basic_auth import BasicAuth
-        auth = BasicAuth()
-
     app.run(host=host, port=port)
