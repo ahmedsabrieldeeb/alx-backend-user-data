f26e3fe2cc024c82d1a4a36c6614511192b0e9d7
Ahmed Eldeeb <eldeeb757575@gmail.com>
2024-09-15 21:54:45 +0300

    feat: Add SessionAuth class for session-based authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Module of SessionAuth class """
+
+from .auth import Auth
+import base64
+
+from typing import Tuple, TypeVar
+
+
+class SessionAuth(Auth):
+    pass
